15fc20b5a137945f8f50fd61db1385f831cfca49
lstidam2 <lstidam2@ivytech.edu>
2023-09-04 17:50:10 -0400

    nodetut

---
+++ modules.js
@@ -0,0 +1,10 @@
+//import people file in and automatically runs it
+
+const{people, ages}= require('./people');
+
+console.log(people, ages);
+
+//information about operating system 
+const os = require('os');
+
+console.log(os)
---
+++ people.js
@@ -0,0 +1,9 @@
+const people=['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages=[20,25,30,35];
+
+
+//this is what will be imported in the file 
+module.exports={
+    people, ages
+
+}
