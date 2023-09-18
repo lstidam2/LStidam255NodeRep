d2f42b0ce1c0b0aebf1660cb85bb37c549b3ffcf
lstidam2 <lstidam2@ivytech.edu>
2023-09-18 15:09:45 -0400

    tut 5

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require("http");
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((request, response)=> {
-    console.log(request.url, request.method);
+    //lodash
+    const num = _.random(0, 20);
+    //console.log(num);
+
+    const greet = _.once(()=>{
+        console.log('hello');
+    });
+
+    greet();
 
 
     // set header content type, write content to brower, end and send response
@@ -22,7 +31,7 @@ const server = http.createServer((request, response)=> {
             path +='about.html';
             response.statusCode=200;
             break;
-        case '/about-me':
+        case '/about-blah':
             response.statusCode=301;
             response.setHeader('Location', '/about');
             response.end();
